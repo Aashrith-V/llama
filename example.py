a0ae506bfda763d9d35555eab32b753d1d00db98
Aashrith-V <v.aashrithkumar@gmail>
2023-03-11 16:49:29 +0530

    making interactive

--- example.py
+++ example.py
@@ -62,6 +62,31 @@ def load(
     print(f"Loaded in {time.time() - start_time:.2f} seconds")
     return generator
 
+def load_the_model(    
+    ckpt_dir: str,
+    tokenizer_path: str,
+    max_seq_len: int = 512,
+    max_batch_size: int = 32,
+):
+    local_rank, world_size = setup_model_parallel()
+    if local_rank > 0:
+        sys.stdout = open(os.devnull, "w")   
+    generator = load(
+        ckpt_dir, tokenizer_path, local_rank, world_size, max_seq_len, max_batch_size
+    )
+    return generator
+
+def infer_the_model(
+    generator, 
+    prompts,
+    max_gen_len: int = 256,
+    temperature: float = 0.8,
+    top_p: float = 0.95,
+):
+    results = generator.generate(
+        prompts, max_gen_len=max_gen_len, temperature=temperature, top_p=top_p
+    )
+    return results
 
 def main(
     ckpt_dir: str,
@@ -70,45 +95,12 @@ def main(
     top_p: float = 0.95,
     max_seq_len: int = 512,
     max_batch_size: int = 32,
+    max_gen_len: int = 256
 ):
-    local_rank, world_size = setup_model_parallel()
-    if local_rank > 0:
-        sys.stdout = open(os.devnull, "w")
-
-    generator = load(
-        ckpt_dir, tokenizer_path, local_rank, world_size, max_seq_len, max_batch_size
-    )
-
-    prompts = [
-        # For these prompts, the expected answer is the natural continuation of the prompt
-        "I believe the meaning of life is",
-        "Simply put, the theory of relativity states that ",
-        "Building a website can be done in 10 simple steps:\n",
-        # Few shot prompts: https://huggingface.co/blog/few-shot-learning-gpt-neo-and-inference-api
-        """Tweet: "I hate it when my phone battery dies."
-Sentiment: Negative
-###
-Tweet: "My day has been 👍"
-Sentiment: Positive
-###
-Tweet: "This is the link to the article"
-Sentiment: Neutral
-###
-Tweet: "This new music video was incredibile"
-Sentiment:""",
-        """Translate English to French:
-
-sea otter => loutre de mer
-
-peppermint => menthe poivrée
-
-plush girafe => girafe peluche
-
-cheese =>""",
-    ]
-    results = generator.generate(
-        prompts, max_gen_len=256, temperature=temperature, top_p=top_p
-    )
+    generator = load_the_model(ckpt_dir, tokenizer_path, max_seq_len, max_batch_size)
+    prompt = input("enter prompt >")
+    prompts = [prompt]
+    results = infer_the_model(generator, prompts, max_gen_len, temperature, top_p)
 
     for result in results:
         print(result)
