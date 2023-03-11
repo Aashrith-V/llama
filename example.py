a202f8e671bbe9a6fa1df2d3b92709f2954258fb
Aashrith-V <v.aashrithkumar@gmail>
2023-03-11 22:52:44 +0530

    32

--- example.py
+++ example.py
@@ -95,7 +95,7 @@ def main(
     temperature: float = 0.8,
     top_p: float = 0.95,
     max_seq_len: int = 256,
-    max_batch_size: int = 32,
+    max_batch_size: int = 512,
     max_gen_len: int = 256
 ):
 
